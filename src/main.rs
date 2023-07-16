1a56f004ef632559137de6be7f734730754d6c17
conor <conormckechnie234@gmail.com>
2023-07-17 00:58:08 +0100

    Working sphere raycaster

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
-mod engine;
+mod scene;
 
-use engine::canvas::Canvas;
 use minifb::Key;
+use scene::engine::{Scene, Sphere, Vector3d};
 
-const WIDTH: usize = 1280;
+const WIDTH: usize = 800;
 const HEIGHT: usize = 800;
 
 fn main() {
-    let mut canvas: Canvas = Canvas::new(WIDTH, HEIGHT);
-    let mut counter: u32 = 0;
+    let sp = Sphere {
+        radius: 1.0,
+        centre: Vector3d {
+            x: 0.0,
+            y: 0.0,
+            z: 10.0,
+        },
+        color: 0xFF00,
+    };
+
+    let mut scene = Scene::new(WIDTH, HEIGHT, vec![sp]);
+
+    let v = Vector3d {
+        x: 3.0,
+        y: 2.0,
+        z: 10.0,
+    };
 
     // Limit to max ~60 fps update rate
-    canvas
+    scene
+        .canvas
         .window
         .limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    while canvas.window.is_open() && !canvas.window.is_key_down(Key::Escape) {
-        for x in -640..640 {
-            for y in -400..400 {
-                canvas.put_pixel(x, y, counter);
-                counter += 1;
-            }
-        }
+    scene.draw_scene();
 
+    while scene.canvas.window.is_open() && !scene.canvas.window.is_key_down(Key::Escape) {
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        canvas.update();
+        scene.canvas.update();
     }
 }
---
+++ src/scene/engine.rs
@@ -0,0 +1,229 @@
+use minifb::{Window, WindowOptions};
+use std::{
+    f64::INFINITY,
+    ops::{Add, Mul, Sub},
+};
+
+type Color = u32;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Vector3d {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Add for Vector3d {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Sub for Vector3d {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vector3d {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl Vector3d {
+    pub fn dot(&self, other: &Self) -> f64 {
+        return (self.x * other.x) + (self.y * other.y) + (self.z * other.z);
+    }
+}
+
+struct Viewport {
+    width: f64,
+    height: f64,
+    distance: f64,
+}
+
+impl Viewport {
+    pub fn default() -> Self {
+        Viewport {
+            width: 1.0,
+            height: 1.0,
+            distance: 1.0,
+        }
+    }
+}
+
+/// A very simple canvas that can be drawn to and rendered
+pub struct Canvas {
+    pub width: usize,
+    pub height: usize,
+    pub window: Window,
+    buffer: Vec<u32>,
+}
+
+impl Canvas {
+    pub fn new(width: usize, height: usize) -> Self {
+        Canvas {
+            width,
+            height,
+            buffer: vec![0; width * height],
+            window: Window::new("Hello", width, height, WindowOptions::default()).unwrap_or_else(
+                |e| {
+                    panic!("{}", e);
+                },
+            ),
+        }
+    }
+
+    /// Put the color at the coordinate given by (x, y) using normal coordinates.
+    /// i.e (0,0) is the pixel in the centre of the screen.
+    pub fn put_pixel(&mut self, x: i32, y: i32, color: u32) {
+        let new_x = x + (self.width as i32) / 2;
+        let new_y = y + (self.height as i32) / 2;
+
+        if new_x < 0 || new_x >= self.width as i32 || new_y < 0 || new_y >= self.height as i32 {
+            // Coordinates are out of bounds (will crash if we try to use these as buffer coords)
+            return;
+        }
+
+        self.buffer[new_y as usize * self.width + new_x as usize] = color;
+    }
+
+    /// Draw the current buffer to the screen.
+    /// Call this to make your changes actually do something.
+    pub fn update(&mut self) {
+        self.window
+            .update_with_buffer(&self.buffer, self.width, self.height)
+            .unwrap();
+    }
+}
+
+pub struct Sphere {
+    pub centre: Vector3d,
+    pub radius: f64,
+    pub color: Color,
+}
+
+/// The entrypoint class for the engine, encapsulates all entities and main classes needed to raycast a scene.
+/// The internal canvas is where the actual pixels will reside after drawing the scene.
+pub struct Scene {
+    spheres: Vec<Sphere>,
+    origin: Vector3d,
+    viewport: Viewport,
+    pub canvas: Canvas,
+}
+
+impl Scene {
+    pub fn new(width: usize, height: usize, spheres: Vec<Sphere>) -> Scene {
+        Scene {
+            origin: Vector3d {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            viewport: Viewport::default(),
+            canvas: Canvas::new(width, height),
+            spheres: spheres,
+        }
+    }
+
+    /// Render the provided vector of renderable items to its internal canvas,
+    /// You still need to update the canvas for it to show the changes.
+    pub fn draw_scene(&mut self) {
+        for x in -(self.canvas.width as i32) / 2..(self.canvas.width as i32) / 2 {
+            for y in -(self.canvas.height as i32) / 2..(self.canvas.height as i32) / 2 {
+                let D = self.canvas_to_viewport(x as f64, y as f64);
+                let color = self.trace_ray(self.origin, D, 1.0, INFINITY);
+
+                // println!("x={:?}, y={:?}, color={:?}", x, y, color);
+
+                self.canvas.put_pixel(x, y, color);
+            }
+        }
+    }
+
+    fn viewport_x(&self, x: f64) -> f64 {
+        x * (self.viewport.width / self.canvas.width as f64)
+    }
+
+    fn viewport_y(&self, y: f64) -> f64 {
+        y * (self.viewport.height / self.canvas.height as f64)
+    }
+
+    fn canvas_to_viewport(&self, x: f64, y: f64) -> Vector3d {
+        Vector3d {
+            x: self.viewport_x(x),
+            y: self.viewport_y(y),
+            z: self.viewport.distance,
+        }
+    }
+
+    fn trace_ray(&self, O: Vector3d, D: Vector3d, t_min: f64, t_max: f64) -> Color {
+        let mut closest_t = INFINITY;
+        let mut closest_sphere = Option::<&Sphere>::None;
+
+        for sphere in self.spheres.iter() {
+            let (t1, t2) = self.intersect_ray_with_sphere(O, D, sphere);
+
+            // println!("t1={:?}, t2={:?}", t1, t2);
+
+            if (t_min < t1) && (t1 < t_max) && (t1 < closest_t) {
+                closest_t = t1;
+                closest_sphere = Some(sphere);
+            }
+
+            if (t_min < t2) && (t2 < t_max) && (t2 < closest_t) {
+                closest_t = t2;
+                closest_sphere = Some(sphere);
+            }
+        }
+
+        if let Some(sp) = closest_sphere {
+            return sp.color;
+        } else {
+            return 0xFFFFFF; // nothing, void
+        }
+    }
+
+    fn intersect_ray_with_sphere(&self, O: Vector3d, D: Vector3d, sphere: &Sphere) -> (f64, f64) {
+        let CO: Vector3d = O - sphere.centre;
+
+        let a = D.dot(&D);
+        let b = 2.0 * CO.dot(&D);
+        let c = CO.dot(&CO) - sphere.radius * sphere.radius;
+
+        let discriminant = b * b - 4.0 * a * c;
+
+        /* println!(
+            "CO={:?}, D = {:?}, a = {:?}, b = {:?}, c={:?}, discriminant={:?}",
+            CO, D, a, b, c, discriminant
+        ); */
+
+        if discriminant < 0.0 {
+            return (f64::INFINITY, f64::INFINITY);
+        }
+
+        let t1 = (-b + discriminant.sqrt()) / (2.0 * a);
+        let t2 = (-b - discriminant.sqrt()) / (2.0 * a);
+
+        (t1, t2)
+    }
+}
